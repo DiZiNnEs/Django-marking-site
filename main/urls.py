b8de1bc6e6b50ad5a7c6f596a61ceca33409bc6c
DiZiNnEs <dizinnes228@mail.ru>
2020-02-17 08:29:06 +0600

    More updates

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('home/', views.index, name='home'),
+    path('', views.index, name='main'),
+    path('home/', views.home, name='home'),
     path('add/', views.add, name='add'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about')
 ]
-
--- main/views.py
+++ main/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'main/index.html')
+
+
+def home(request):
+    return render(request, 'main/home.html')
+
 
 def add(request):
     return render(request, 'main/add.html')
 
+
 def contact(request):
     return render(request, 'main/contact.html')
+
 
 def about(request):
     return render(request, 'main/about.html')
